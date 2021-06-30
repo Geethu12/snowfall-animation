8f7f7d725e2c86c7eaa628492371a9eda72c0a2a
Geethu12 <83004591+Geethu12@users.noreply.github.com>
2021-06-30 11:33:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+var snow=[];
+function preload(){
+  bg =loadImage("snow3.jpg");
+}
+function setup() {
+
+  createCanvas(800,400);
+  engine = Engine.create();
+  world = engine.world;
+  a=new snowflakes(200,200,10);
+}
+
+function draw() {
+  background(bg);  
+  Engine.update(engine);
+  snow.push(new snowflakes(random(0,800),0,50));
+  for(var i=0;i<snow.length;i++){
+  snow[i].display();
+  }
+}
